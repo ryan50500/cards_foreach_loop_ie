a25b92ae8cd7e8039e2046c5dbc416c8ab1dfd31
ryan woods <federer5050@gmail.com>
2021-03-05 13:21:10 +0000

    commenting for clearer understanding

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
+        // removing active class from all cards first
         removeActiveClasses()
+        // then add active to the clicked card
         panel.classList.add('active')
     })
 })
