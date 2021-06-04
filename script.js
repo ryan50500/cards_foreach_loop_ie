51b5d1e1d15a7cc714d6bdd66d25781d8fd7ab80
ryan woods <federer5050@gmail.com>
2021-06-04 09:53:23 +0100

    fix internet exploer

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 const panels = document.querySelectorAll('.panel')
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
+
+
+
+// (es6 not compatible for IE)
+
+// panels.forEach(panel => {
+//     panel.addEventListener('click', () => {
+//         // removing active class from all cards first
+//         removeActiveClasses()
+//         // then add active to the clicked card
+//         panel.classList.add('active')
+//     })
+// })
+
+
+
+
+
+// (array prototype fix for IE)
+
+Array.prototype.slice.call(panels).forEach(function(panel) {
+    panel.addEventListener('click', function() {
         // removing active class from all cards first
         removeActiveClasses()
         // then add active to the clicked card
         panel.classList.add('active')
     })
 })
+
 
 function removeActiveClasses() {
     panels.forEach(panel => {
